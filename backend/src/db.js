b30488c5c3eecb314f5c812989e7ae73d478babb
operman-code <jagtapomkar3052002@gmail.com>
2025-11-30 21:09:46 +0530

    Update db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -2,7 +2,7 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'postgres',   // <-- FIXED
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASS || 'password',
   database: process.env.DB_NAME || 'taskflow',
